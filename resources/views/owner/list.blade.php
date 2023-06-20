35a9281e335ca6b26088125a034aa96a55ae7175
henriquensco <henriquensco@gmail.com>
2023-06-20 10:17:13 -0300

    Add: styles to owners list

--- resources/views/owner/list.blade.php
+++ resources/views/owner/list.blade.php
@@ -8,16 +8,40 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <a href="/owner/create" class="p-2 dark:bg-gray-200 rounded">New Properties's Owner</a>
-            
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                @foreach ($owners['data'] as $owner)
-                    <div class="flex gap-2 mb-4 dark:text-gray-200 text-gray-800">
-                        <span class="mr-2">Full Name: {{ $owner['full_name'] }}</span>
-                        <span class="mr-2">Occupation: {{ $owner['occupation'] }}</span>
-                        <span>CPF: {{ $owner['cpf'] }}</span>
-                    </div>
-                @endforeach
+
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg w-full">
+                <table class="w-full">
+                    <thead>
+                        <tr>
+                            <th>Full Name</th>
+                            <th>Occupation</th>
+                            <th>CPF</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($owners['data'] as $owner)
+                            <tr class="text-center">
+                                <td class="mr-2">{{ $owner['full_name'] }}</td>
+                                <td class="mr-2">{{ $owner['occupation'] }}</td>
+                                <td>{{ $owner['cpf'] }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 </x-app-layout>
+
+<style>
+    td,
+    th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {
+        background-color: #dddddd;
+    }
+</style>
